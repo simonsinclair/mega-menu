3d7e84b1d9c51a1fc12f1fe5150fbd4530fd6645
Simon Sinclair <simon@sinclair>
2015-05-14 16:19:36 +0100

    Update

--- public/js/main.js
+++ public/js/main.js
@@ -13,16 +13,24 @@
 
     bindEvts: function() {
 
-      $("#js-primary-nav").on('click', 'a', Menu.onPrimaryNavClick);
+      $("#js-primary-nav").on('click', 'a:not(#js-all-news)', Menu.onPrimaryNavClick);
+      $("#js-all-news").on('click', Menu.onAllNewsClick);
     },
 
     onPrimaryNavClick: function(e) {
       e.preventDefault();
 
-      $("#js-primary-nav li").removeClass('active');
-      $(this)
-        .parent()
-        .addClass('active');
+      if( ! $(this).parent().hasClass('active') ) {
+
+        $("#js-primary-nav li").removeClass('active');
+        $(this)
+          .parent()
+          .addClass('active');
+      }
+    },
+
+    onAllNewsClick: function(e) {
+      e.preventDefault();
     }
   };
 
